de3471280b0c096e9984cc01ce4703da2fe9a992
Samandar <86640731+samandaru@users.noreply.github.com>
2021-08-22 00:46:20 +0900

    stylecomponent

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import { Wrapper } from './style'
+
+export default class App extends Component {
+  render() {
+    return <Wrapper>text</Wrapper>;
+      
+    
+  }
+}
---
+++ src/style.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+
+export const Wrapper = styled.div`
+display:flex;
+width:200px;
+height: 200px;
+box-shadow: 0 0 8px blue;
+margin: 100px;
+`;
+
+
